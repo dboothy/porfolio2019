875f71996de301845ee69db9a67eb854d3e42167
David Booth-Metwally <davidbmetwally@gmail.com>
2019-01-27 18:23:04 -0800

    eddited css and added materialize css stylesheet and edited

---
+++ assets/app.js
@@ -0,0 +1,26 @@
+var elem = document.querySelector('.tabs');
+     var options = {}
+     var instance = M.Tabs.init(elem, options);
+  
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+
+
+
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+
+    
+  var instance = M.Collapsible.getInstance(elem);
+
+  /* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+
+    $('.collapsible').collapsible('methodName');
+    $('.collapsible').collapsible('methodName', paramName);
+  */
+      
+  });
